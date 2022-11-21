4eff846eb7809abdd55fe0f96865cc5736b9b636
lienapaeps <paepsliena@gmail.com>
2022-11-21 21:21:21 +0100

    feat: create vite project with vuejs

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>// logica
+
+</script>
+
+<template>
+  <div class="app">
+    Dit is de frontend
+  </div>
+</template>
+
+<style scoped>
+.app {
+  background-color: red;
+}
+</style>
