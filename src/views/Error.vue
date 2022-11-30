9d356a212ef701831e7924e4d6155aa789b1e45a
lienapaeps <paepsliena@gmail.com>
2022-11-30 11:38:43 +0100

    feat: add all vue pages

---
+++ src/views/Error.vue
@@ -0,0 +1,15 @@
+<script setup>// logica
+
+</script>
+
+<template>
+    <div class="app">
+        Dit is de vuejs error
+    </div>
+</template>
+
+<style scoped>
+.app {
+    background-color: red;
+}
+</style>
---
+++ src/views/OrderOk.vue
@@ -0,0 +1,15 @@
+<script setup>// logica
+
+</script>
+
+<template>
+    <div class="app">
+        Dit is de vuejs order ok
+    </div>
+</template>
+
+<style scoped>
+.app {
+    background-color: red;
+}
+</style>
