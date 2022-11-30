b4d9990f153e63aac48a52aa67549e1187bfb04f
lienapaeps <paepsliena@gmail.com>
2022-11-30 11:40:43 +0100

    feat: add login.vue

---
+++ src/views/LogIn.vue
@@ -0,0 +1,15 @@
+<script setup>// logica
+
+</script>
+
+<template>
+    <div class="app">
+        Dit is de vuejs login
+    </div>
+</template>
+
+<style scoped>
+.app {
+    background-color: red;
+}
+</style>
