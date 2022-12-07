c9ada4e2edcacc8cf180b09f1a87a6498773cd59
lienapaeps <paepsliena@gmail.com>
2022-12-07 14:17:17 +0100

    feat: add blender donut

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,8 @@ import Home from './views/Home.vue'
   <!-- <LogIn /> -->
   <!-- <Home /> -->
   <!-- <Button text="" class="btn shoppingcart" textclass="btn__text"/> -->
-    <!-- <Button text="Maak Donut" class="btn btn--strawberry" textclass="btn__text"/> -->
-    <!-- <Shoppingbutton/> -->
+  <!-- <Button text="Maak Donut" class="btn btn--strawberry" textclass="btn__text"/> -->
+  <!-- <Shoppingbutton/> -->
   <!-- <Gallery /> -->
   <!-- <LogIn /> -->
 </template>
