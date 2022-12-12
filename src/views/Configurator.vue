9076520940372fb0175b3de1a2ee30ede66f2a9a
marthemaere <marthemaere@gmail.com>
2022-12-12 14:25:14 +0100

    "feat: add order form"

--- src/views/Configurator.vue
+++ src/views/Configurator.vue
@@ -1,5 +1,6 @@
 <script setup>
     import Donut from '../components/Donut.vue'
+    import OrderForm from '../components/OrderForm.vue'
 </script>
 
 <template>
@@ -58,6 +59,9 @@
                 <input type="radio" name="logo" id="oval" value="oval">
                 <label for="oval" class="logo logo--oval"></label>
             </div>
+        </div>
+        <div class="order">
+            <OrderForm/>
         </div>
     </body>
 </template>
