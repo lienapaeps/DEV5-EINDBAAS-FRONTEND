6b2482b6e798c6c26dcdab67b094a82d8dfce159
lienapaeps <paepsliena@gmail.com>
2022-12-20 22:01:16 +0100

    feat: update status of donut

--- src/views/DonutDetails.vue
+++ src/views/DonutDetails.vue
@@ -9,6 +9,23 @@ let donuts = reactive({
 let donutId = window.location.pathname.split("/")[2];
 // console.log(id);
 
+// update status from donut when button is clicked
+const updateStatus = (status) => {
+    if (localStorage.getItem("token")) {
+        fetch('https://dev5-donuttello.onrender.com/api/v1/donuts/' + donutId, {
+            'method': 'PUT',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+            'body': JSON.stringify({
+                status: status
+            })
+        }).then(res => res.json())
+        console.log("Status is aangepast");
+    }
+}
+
 onMounted(() => {
     if (localStorage.getItem("token")) {
         fetch("https://dev5-donuttello.onrender.com/api/v1/donuts/" + donutId, {
@@ -16,8 +33,7 @@ onMounted(() => {
             headers: {
                 "Content-Type": "application/json",
                 "authorization": "Bearer " + localStorage.getItem("token"),
-            },
-            mode: 'cors',
+            }
         })
             .then(response => response.json())
             .then(json => {
@@ -26,7 +42,9 @@ onMounted(() => {
                 donuts.donuts = json.data.donut;
             })
     }
-})
+});
+
+
 
 </script>
 
@@ -48,6 +66,12 @@ onMounted(() => {
             <p>Bestel datum: <span>{{ donut.createdAt }}</span></p>
             <p>Opmerkingen: <span>{{ donut.remarks }}</span></p>
             <p>Status: <span>{{ donut.status }}</span></p>
+            <h3>Status wijzigen</h3>
+            <div class="status__options">
+                <button v-on:click="updateStatus('Opgeslagen')" class="btn--status">Opgeslagen</button>
+                <button v-on:click="updateStatus('In productie')" class="btn--status">In productie</button>
+                <button v-on:click="updateStatus('Klaar')" class="btn--status">Klaar</button>
+            </div>
         </div>
         <div class="details__order">
             <h2>Bedrijfsgevens</h2>
@@ -112,6 +136,26 @@ span {
     max-width: 100%;
 }
 
+h3 {
+    font-weight: 500;
+    margin-top: 1em;
+}
+
+.status__options {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1.5em;
+    margin-top: 1em;
+}
+
+.btn--status {
+    padding: 1em;
+    border: 1px solid #dee2e6;
+    border-radius: 8px;
+}
+
+
 /* Tablet */
 @media (min-width: 768px) {}
 
@@ -140,6 +184,22 @@ span {
 
     .logo__img {
         max-width: 20%;
+    }
+
+    .status__options {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        gap: 1.5em;
+    }
+
+    .status__options button {
+        cursor: pointer;
+    }
+
+    .status__options button:hover {
+        background-color: var(--strawberry-pink);
+        color: var(--lemon-yellow)
     }
 }
 </style>
